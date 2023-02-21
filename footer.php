13018b7b7a04b1ab3ad31f1151e4eb21dd6468a0
Firelag <jordan.vincent085@gmail.com>
2023-02-21 11:19:59 +0100

    commit 6

--- footer.php
+++ footer.php
@@ -2,5 +2,19 @@
 
 
 <footer>
-    
+    <div class="flex">
+    <img src="http://localhost/ItalianTaste/wp-content/uploads/2023/02/1777015e11cf4a4d8ab93e96720491f7.png" alt="logo">
+    <div class="res">
+        <p>SUIVEZ-NOUS SUR LES RÉSEAUX SOCIAUX</p>
+        <div class="logo">
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+
+
+        </div>
+        
+    </div>
+    </div>
+<p>© 2023 - Copyright Italian Taste/Tous droits réservés </p>
 </footer>
