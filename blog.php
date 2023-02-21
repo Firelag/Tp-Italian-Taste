d653870fdf8b2f138723cde5c0b4f7512e079d37
Firelag <jordan.vincent085@gmail.com>
2023-02-21 12:16:48 +0100

    commit 8

--- blog.php
+++ blog.php
@@ -1,6 +1,11 @@
 <?php get_header();
 //Template Name: Blog
 ?>
+
+<div class="pagehero">
+    <?php $thumb = get_the_post_thumbnail_url(); ?>
+        <div id="hero" style="background-image: url('<?php echo $thumb;?>')">
+</div>
 <main id="blog">
 <div class="container">
 <h2><?php the_title()?></h2>
@@ -14,6 +19,8 @@ $recentPosts->query('showposts=8');
 <!-- boucle wordpress pour afficher les extraits d'article -->
 <?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
 <h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+<div class="bgimg" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ,'blogpage_thumb' ) ); ?>');">
+            </div>
 <!-- permet d'afficher un extrait d'article -->
 <?php the_excerpt(); ?>
 <!-- fin permet d'afficher un extrait d'article -->
