2d2e6c7c842e5aeaeb256697393ff0ae1a8291a0
Firelag <jordan.vincent085@gmail.com>
2023-02-21 15:19:34 +0100

    commit 10

--- contact.php
+++ contact.php
@@ -5,7 +5,23 @@
     <?php $thumb = get_the_post_thumbnail_url(); ?>
         <div id="hero" style="background-image: url('<?php echo $thumb;?>')">
 </div>
+<section class="contact">
+    <div class="flex">
+        <div>
+            <h3>Nous contacter</h3>
+            <div>
+                <p>ItalianTaste@gmail.com</p>
+            </div>
+        </div>
+        
 
+        <?php the_content();?>
+    </div>
+   
+
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d768.7543714130238!2d2.334439434824142!3d48.88906392781652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e5084692565%3A0x9ebb8875dc2a4720!2sSquare%20Caulaincourt!5e0!3m2!1sfr!2sfr!4v1676987971341!5m2!1sfr!2sfr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</section>
 
 
 <?php get_footer(); ?>
